4514baec6b09e5076a79ced9d2ea71a0d642dae7
RandoVanikov <randovanikov@gmail.com>
2023-12-14 22:46:33 +0200

    functions

---
+++ ArgumentsAndParameters/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//userInput == Argument
+//anyString == parameter
+
+
+Console.WriteLine("Enter a word:");
+
+String userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); 
+
+static void PrintAnyWord(string anyString)
+{
+   anyString = anyString.ToUpper();
+    for(int i = 0; i < 5 ; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ Week7_Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Are you here? (yes/no):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "yes")
+{
+    PrintHello();
+
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+static void PrintHello()
+{
+
+Console.WriteLine("Hello, World!");
+
+}
+
+
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("Good bye, see you later!");
+
+}
